0d2eefcf939824e91b2d3415ca8e30cc7b65be97
Sofia Medina <sofiamedina0407@gmail.com>
2017-09-15 02:55:58 -0500

    plot in function

--- VEP_analysis2.m
+++ VEP_analysis2.m
@@ -1,19 +1,16 @@
-function [ VEPdata,TData, P100delay] = VEP_analysis2( senal2nueva, estimulo2nuevo,tvectnuevo, rango)
+function [ VEPdata,TData, P100delay, NoPot, Nreal] = VEP_analysis2( senal2nueva, estimulo2nuevo,tvectnuevo, rango)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
-
-%Time variables
-tP100=100;
-tN75=75;
-tN145=145;
 
 
 
 [Ax,Ix] = findpeaks( estimulo2nuevo,'MinPeakHeight',3);
 Cont=length(Ax);
+Nreal=Cont;
 VEPdata=zeros(Cont,6);
-P100delay=zeros(Cont,1);
+NoPot=zeros(Cont,1);
 TData=zeros(Cont,3);
+P100delay=zeros(Cont,1);
 
 for i =1:1:Cont
     
@@ -27,7 +24,8 @@ for i =1:1:Cont
     estTime=tvectnuevo(index);
     %Find P100
     [AxP100,IxP100] = min(senal2nueva(index:indexsup));
-    
+    if(AxP100<-10)
+         
     IxP100=IxP100+index;
     txP100=tvectnuevo(IxP100);
     TData(i,2)=txP100-estTime;
@@ -49,7 +47,19 @@ for i =1:1:Cont
 
     %Calculate delays 
 
-    P100delay(i,1)=estTime-txP100;
+    P100delay(i,1)=estTime-txP100;  
+    NoPot(i)=0;
+    
+    else
+        
+      VEPdata(i,:) =0;
+      NoPot(i)=1;
+      Nreal=Nreal-1;
+      TData(i)=0;
+      
+    
+    end
+
 
 end
 
