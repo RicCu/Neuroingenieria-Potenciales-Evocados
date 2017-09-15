63db2ed6e5b576c7e096393835288a668c850891
Sofia Medina <sofiamedina0407@gmail.com>
2017-09-15 04:56:19 -0500

    Final

--- testMain.m
+++ testMain.m
@@ -1,5 +1,5 @@
 close all;
-clear all;
+
 %Frecuency variables
 fs=50000; %Sampling frequency
 fd=2000; %Desired frequency
@@ -15,7 +15,8 @@ dlyP100=0;
 rango=samplesms*50; %buscar en los próximos 50 ms
 
 [ estimulo, senal] = Get_file();
-
+tic 
+tStart=tic ;
 
 [senal2, estimulo2 ] = Process_Signal( senal,estimulo, fs, fd);
 %% Create long vector
@@ -29,7 +30,7 @@ rango=samplesms*50; %buscar en los próximos 50 ms
 % 
 % t=length(senal2nueva)/fd;
 % t2=length(estimulo2nuevo)/fd;
-% tvect=1000*(1:length(senal2))/fd; %Create time vector and change to ms
+ tvect=1000*(1:length(senal2))/fd; %Create time vector and change to ms
 % tvectnuevo=1000*(1:length(senal2nueva))/fd; %Create time vector and change to ms
 [ senal2nueva, estimulo2nuevo,tvectnuevo] = create_longvector(senal2,estimulo2,samplesms,fd );
 figure
@@ -44,25 +45,29 @@ plot(tvectnuevo,estimulo2nuevo)
 %% Get events of signal
 %infgraph, infreal, dlyP100] = VEP_analysis( senal2nueva, estimulo2nuevo,tvectnuevo, rango);
 %[ infgraph, infreal, dlyP100] = VEP_analysis( senal2, estimulo2,tvect, rango);
-[ VEPdata,TData, P100delay, NoPot, Nreal] = VEP_analysis2( senal2nueva, estimulo2nuevo,tvectnuevo, rango);
+
+[ VEPdata,TData, P100delay, NoPot, Nreal] = VEP_analysis2( senal2, estimulo2,tvect, rango);
 VEPdataMean=sum(TData)/Nreal;
  
 %% Plot results
 %subplot(2,1,1); plot(tvect,senal2,'*','MarkerIndices',[IxP100 I75 I145]);subplot(2,1,2); plot(t2vect,estimulo2);
 figure
 subplot(2,1,1); 
-plot(tvectnuevo,senal2nueva); hold on
-for i = 1:1:5
-plot(VEPdata(i,1),VEPdata(i,2),'*',VEPdata(i,3),VEPdata(i,4),'*',VEPdata(i,5),VEPdata(i,6),'*' );
-hold on 
-end
+%plot(tvectnuevo,senal2nueva); hold on
+plot(tvect,senal2); hold on
+% for i = 1:1:5
+% plot(VEPdata(i,1),VEPdata(i,2),'*',VEPdata(i,3),VEPdata(i,4),'*',VEPdata(i,5),VEPdata(i,6),'*' );
+% hold on 
+% end
 
 set(gca, 'XGrid', 'on' );
 set(gca,'GridLineStyle',':');
-subplot(2,1,2); plot(tvectnuevo,estimulo2nuevo);
+%subplot(2,1,2); plot(tvectnuevo,estimulo2nuevo);
+subplot(2,1,2); plot(tvect,estimulo2);
 
 set(gca, 'XGrid', 'on' ); % To do.. change trasparency
 set(gca,'GridLineStyle',':');
 
+tElapsed = toc(tStart); 
 %size=length(senal);
 %fct= factores(size);
