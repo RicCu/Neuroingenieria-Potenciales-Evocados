3d6ce10963198b9cf635049d0211a5d8f46926dd
Sofia Medina <sofiamedina0407@gmail.com>
2017-11-17 10:49:50 -0600

    Changed image parameters

--- TestLoop.m
+++ TestLoop.m
@@ -2,9 +2,9 @@ s = 0;
 c0 = -1;
 c1 = -1;
 img = -1;
-size = 2;
+size = 4;
 [c0, c1, img] = StimulateVision(s, c0, c1, img, size);
-for i = 0:10
+for i = 0:150
     s = xor(s, 1);
     %tic
     [c0, c1, img] = StimulateVision(s, c0, c1, img);
