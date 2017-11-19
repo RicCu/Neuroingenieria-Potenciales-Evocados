2a63412713e217c62051fb1e510ea25a4f827e08
SofiaMedina <sofiamedina0407@gmail.com>
2017-11-19 14:37:22 -0600

    ...

--- CreateSequence.m
+++ CreateSequence.m
@@ -3,34 +3,45 @@ function CreateSequence (time, seq, tipo)
 %tipo=1 cuando solo quire pantalla negra y un checkboard
 %tipo=2 cuando quiere pantalla negra y dos checkboard ... se puede cambiar
 %si se desean mas checkboards
-
+global data
 dWidth = 1; %Inicializacion de valores para Checkboard
 dLength = 5;
 NumEstim=length(seq);
 Sequence=cell(NumEstim, 3); %Matriz donde se guardarán datos de CreateCheckboard:  [ chb0, chb1, black ]
 img = -1;
-blacktime= NumEstim+1; %Empieza con pantalla negra un segundo luego un segundo despues de cada checkboard
+blacktime= NumEstim; %Empieza con pantalla negra un segundo luego un segundo despues de cada checkboard
 fs=500;
 d = daq.getDevices;
 s = daq.createSession('ni');
 addAnalogInputChannel(s,'Dev2',0, 'Voltage');
 s.Rate = fs;
-s.DurationInSeconds=(NumEstim*tipo*time)+blacktime;
-lh=addlistener(s,'DataAvailable',@(src,event)StoreData2(src, event, size));
+TotalTime=(NumEstim*tipo*time)+blacktime;
+s.DurationInSeconds=TotalTime;
+lh=addlistener(s,'DataAvailable',@(src,event)StoreData2(src, event));
 
-for i= 1:1:NumEstim
-  
+s.NotifyWhenDataAvailableExceeds = fs*TotalTime;
+startBackground(s);
+
+i=1;
+while(~s.IsDone)
+ 
+%for i= 1:1:NumEstim
+%  if tipo==1
  [ Sequence{i,1},Sequence{i,2},Sequence{i,3}] =   CreateCheckboard( seq(1,i), dWidth, dLength ); %[ chb0, chb1, black ] = CreateCheckboard(  size, dWidth, dLength )
  [c0, c1 , img]=StimulateVision(0, Sequence{i,3}, Sequence{i,1}, img);
- pause(2)
+ pause(1)
  [c0, c1 , img]=StimulateVision(0, Sequence{i,1}, Sequence{i,1}, img);
-  pause(2)
+  pause(time)
  [c0, c1 , img]=StimulateVision(0, Sequence{i,2}, Sequence{i,1}, img);
-   pause(2)
-   
+  pause(time)
+  i=i+1;
+
+  
+%end
 end
 
-
+wait(s);
+delete(lh);
 
 
 end
--- StoreData2.m
+++ StoreData2.m
@@ -1,21 +1,16 @@
-function StoreData2(src, evt, size)
+function StoreData2(src, evt)
 %n=s.ScansAquired;
 global data
-global s2
-global c0
-global c1
-global img
-global cumAvg
-global n_cum
+
 %tic
 tempData= evt.Data;
 %tempData=[evt.TimeStamps, evt.Data];
 n=src.ScansAcquired;
 %p=(n/250)*2;
 
-p=(n/2000);
+%p=(n/2000);
 %data(:,p-1:p)=tempData;
-data(:,p)=tempData;
+data=tempData;
 %[cumAvg, n_cum] = cum_avg(tempData', cumAvg, n_cum);
 %figure
 %plot(evt.Data)
