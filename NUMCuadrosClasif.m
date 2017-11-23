ef9070a6d67805efac6e808174af1fef3e7966d9
Sofia Medina <sofiamedina0407@gmail.com>
2017-11-23 13:13:49 -0600

    análisis de Pablo Y Rich

--- NUMCuadrosClasif.m
+++ NUMCuadrosClasif.m
@@ -1,23 +1,100 @@
 close all
 clear all
 
-data4=csvread('clasificadorcanal2:4seg32cuad20Estim.csv');
-data32=csvread('clasificadorcanal2:4seg4cuad20Estim.csv');
-data4P=csvread('clasificadorcanal2:4seg4cuad15EstimPAB.csv');
-data32P=csvread('clasificadorcanal2:4seg32cuad15EstimPAB.csv');
+data4=csvread('clasificadorcanal1:4seg34uad20EstimRi.csv');
+data32=csvread('clasificadorcanal1:4seg32cuad20EstimRi.csv');
+
+% data48EST=csvread('clasificadorcanal1:4seg4cuad8EstimPAB.csv');
+% data328EST=csvread('clasificadorcanal1:4seg32cuad8EstimPAB.csv');
+
+data4C2=csvread('clasificadorcanal2:4seg32cuad20EstimRi.csv');
+data32C2=csvread('clasificadorcanal2:4seg34uad20EstimRi.csv');
+
+% data4C28EST=csvread('clasificadorcanal2:4seg4cuad8EstimPAB.csv');
+% data32C28EST=csvread('clasificadorcanal2:4seg32cuad8EstimPAB.csv');
+
+%  x(1,:)=[data4(1,:),data48EST(1,:)];
+%  y(1,:)=[data4(2,:),data48EST(2,:)];
+%  z(1,:)=[data4(3,:),data48EST(3,:)];
+%  scatter3(x,y,z, 'o');
+ %%
+ x(1,:)=data4(1,:);
+ y(1,:)=data4(2,:);
+ z(1,:)=data4(5,:);
+ scatter3(x,y,z, 'o');
+%%
+% xw(1,:)=[data4(4,:),data48EST(4,:)];
+% yw(1,:)=[data4(5,:),data48EST(5,:)];
+% zw(1,:)=[data4(6,:),data48EST(6,:)];
+% hold on
+% scatter3(xw,yw,zw, '*');
+
+xw(1,:)=data4(6,:);
+yw(1,:)=data4(9,:);
+zw(1,:)=data4(10,:);
+hold on
+scatter3(xw,yw,zw, '*');
+%%
+
+ xC2(1,:)=data4C2(1,:);
+ yC2(1,:)=data4C2(2,:);
+ zC2(1,:)=data4C2(5,:);
+ hold on
+ scatter3(xC2,yC2, zC2, '+');
+ 
+%%
+% xwC2(1,:)=[data4C2(4,:), data4C28EST(4,:)];
+% ywC2(1,:)=[data4C2(5,:), data4C28EST(5,:)];
+% zwC2(1,:)=[data4C2(6,:), data4C28EST(6,:)];
+% hold on
+% scatter3(xwC2,ywC2,zwC2, '*');
+
+xwC2(1,:)=data4C2(6,:);
+ywC2(1,:)=data4C2(9,:);
+zwC2(1,:)=data4C2(10,:);
+hold on
+scatter3(xwC2,ywC2,zwC2, '+');
 
 
+%%
+ x2(1,:)=data32(1,:);
+ y2(1,:)=data32(2,:);
+ z2(1,:)=data32(5,:);
+ hold on
+ scatter3(x2,y2,z2, '+');
+ 
+%%
+xw2(1,:)=data32(8,:);
+yw2(1,:)=data32(9,:);
+zw2(1,:)=data32(10,:);
+hold on
+scatter3(xw2,yw2,zw2, '*');
+%%
 
+ x2C2(1,:)=data32C2(1,:);
+ y2C2(1,:)=data32C2(2,:);
+ z2C2(1,:)=data32C2(5,:);
+ hold on
+ scatter3(x2C2, y2C2,z2C2, '+');
+ 
+%%
+xw2C2(1,:)=data32C2(6,:);
+yw2C2(1,:)=data32C2(9,:);
+zw2C2(1,:)=data32C2(10,:);
+hold on
+scatter3(xw2C2,yw2C2,zw2C2, '+');
+
+%%
 % x(1,:)=[data4P(1,:),data32P(1,:)] ;
 % y(1,:)=[data4P(2,:),data32P(2,:)]  ;
 % z(1,:)=[data4P(3,:),data32P(3,:)] ;
 
+% 
+% x(1,:)=[data4(1,:),data32(1,:)] ;
+% y(1,:)=[data4(2,:),data32(2,:)]  ;
+% z(1,:)=[data4(3,:),data32(3,:)] ;
 
-x(1,:)=[data4(1,:),data32(1,:)] ;
-y(1,:)=[data4(2,:),data32(2,:)]  ;
-z(1,:)=[data4(3,:),data32(3,:)] ;
-
-Data4=[x;y;z];
+% Data4=[x;y;z];
 % Caso4(1,1:length(Data4))=1;
 
 
@@ -25,16 +102,16 @@ Data4=[x;y;z];
 % yw(1,:)=[data4P(5,:),data32P(5,:)];
 % zw(1,:)=[data4P(6,:),data32P(6,:)];
 
-xw(1,:)=[data4(4,:),data32(4,:)];
-yw(1,:)=[data4(5,:),data32(5,:)];
-zw(1,:)=[data4(6,:),data32(6,:)];
-
-Data4W=[xw;yw;zw];
+% xw(1,:)=[data4(4,:),data32(4,:)];
+% yw(1,:)=[data4(5,:),data32(5,:)];
+% zw(1,:)=[data4(6,:),data32(6,:)];
+% 
+% Data4W=[xw;yw;zw];
 % Caso4W(1,1:length(Data4W))=1;
-
-x2(1,:)=data32P(1,:);
-y2(1,:)=data32P(2,:);
-z2(1,:)=data32P(3,:);
+% 
+% x2(1,:)=data32P(1,:);
+% y2(1,:)=data32P(2,:);
+% z2(1,:)=data32P(3,:);
 
 % Data32=[x2;y2;z2];
 % 
--- SpectrogramaBlack.m
+++ SpectrogramaBlack.m
@@ -1,15 +1,15 @@
-data=csvread('4ET4size8NE_uno_4secPAB2.csv');
-data=data(1,:);
+data=csvread('4ET32size20NE_uno_4sec.csv');
+data=data(2,:);
 fs=500;
-NumEstim=8;
+NumEstim=20;
 totalSamples=length(data);
 dt=1/fs;
 TimeVECT=(dt:dt:totalSamples/fs);
 plot(TimeVECT,data)
 SMatrix=zeros(NumEstim, 3);
-spectrogram(data(1,1:2000),2000,2,[1:1:80],500,'yaxis')
+spectrogram(data(1,12001:14000),2000,2,[1:1:80],500,'yaxis')
 figure
-spectrogram(data(1,2001:4000),2000,2,[1:1:80],500,'yaxis')
+spectrogram(data(1,14001:16000),2000,2,[1:1:80],500,'yaxis')
 
 for i=1:1:NumEstim 
     cont=i*4000;
@@ -23,7 +23,7 @@ for i=1:1:NumEstim
 end
 
 figure
-spectrogram(estimDATA,2000,2,[1:1:80],500,'yaxis');
+spectrogram(estimDATA,2000,2,[1:1:70],500,'yaxis');
 [spec_raw1, fspec1, tspec1]=spectrogram(estimDATA,2000,2,[1:1:80],500,'yaxis');
 abs_spec_raw1=abs(spec_raw1); 
 
@@ -31,10 +31,11 @@ abs_spec_raw1=abs(spec_raw1);
 del_the1=abs_spec_raw1((1:7),:);      % Separación manal por bandas de EEG (delta+teta/alfa/beta) 
 alpha1=abs_spec_raw1((8:13),:);
 betha1=abs_spec_raw1((14:31),:);
-ABetha=abs_spec_raw1((32:80),:);
+ABetha=abs_spec_raw1((32:59),:);
+BBetha=abs_spec_raw1((61:80),:);
 
 figure
-spectrogram(blackDATA,2000,2,[1:1:80],500,'yaxis');
+spectrogram(blackDATA,2000,2,[1:1:70],500,'yaxis');
 [spec_raw2, fspec2, tspec2]=spectrogram(blackDATA,2000,2,[1:1:80],500,'yaxis');
 abs_spec_raw2=abs(spec_raw2); 
 
@@ -42,33 +43,40 @@ abs_spec_raw2=abs(spec_raw2);
 del_the2=abs_spec_raw2((1:7),:);      % Separación manal por bandas de EEG (delta+teta/alfa/beta) 
 alpha2=abs_spec_raw2((8:13),:);
 betha2=abs_spec_raw2((14:31),:);
-ABetha=abs_spec_raw2((32:80),:);
+ABetha2=abs_spec_raw2((32:59),:);
+BBetha2=abs_spec_raw2((61:80),:);
 
 
-clasificador_array=zeros(6,(NumEstim));
+clasificador_array=zeros(10,(NumEstim));
 
 for i=1:1:(NumEstim)
     del_prom=mean(del_the1(:,i));     % Al tener varias ventas (en el tiempo) se hace el valor promedio de la señal y se guarda en
     alp_prom=mean(alpha1(:,i));       % el vector de datos clasificador_array que contenia puros ceros
     bet_prom=mean(betha1(:,i));
-    
-    
+    ABetha_prom=mean(ABetha(:,i));
+    BBetha_prom=mean(BBetha(:,i));
  
     del_prom2=mean(del_the2(:,i));    
     alp_prom2=mean(alpha2(:,i));       
     bet_prom2=mean(betha2(:,i));
-    clasificador_array(4,i)=del_prom2;
-    clasificador_array(5,i)=alp_prom2;
-    clasificador_array(6,i)=bet_prom2;
+    ABetha_prom2=mean(ABetha2(:,i));
+    BBetha_prom2=mean(BBetha2(:,i));
+    
+    clasificador_array(6,i)=del_prom2;
+    clasificador_array(7,i)=alp_prom2;
+    clasificador_array(8,i)=bet_prom2;
+    clasificador_array(9,i)=ABetha_prom2;
+    clasificador_array(10,i)=BBetha_prom2;
    
     
     clasificador_array(1,i)=del_prom;
     clasificador_array(2,i)=alp_prom;
     clasificador_array(3,i)=bet_prom;
+    clasificador_array(4,i)=ABetha_prom;
+    clasificador_array(5,i)=BBetha_prom;
     
     
 end
-
 
 
 
