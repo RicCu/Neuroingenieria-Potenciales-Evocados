1f23526b9150ed8e3846695dd9d458ed6e4f3380
Alumno GDA <gdaalumno@svcs.itesm.mx>
2017-11-25 20:18:33 -0600

    Ultimo con análisis de señal

--- process_ERP.m
+++ process_ERP.m
@@ -55,12 +55,21 @@ delete(lh);
 release(s)
 % DATA1=transpose(data1);
 % DATA2=transpose(data1);
+%% Análisis señal 
+rango=25;
 
 [s,e]=size(data1);
 
-BlackDATA=data2(:,1); %get sample of black screen data to calculate gain 
-BASEBlackDATA=mean(BlackDATA);
-Gain=BASEBlackDATA/BASEBlackDATA;  %change to baseblack data when gain is zero
+BlackDATA1=data1(:,1); %get sample of black screen data to calculate gain 
+BASEBlackDATA1=mean(BlackDATA1);
+BlackDATA2=data2(:,1); %get sample of black screen data to calculate gain 
+BASEBlackDATA2=mean(BlackDATA2);
+
+REFC1=BASEBlackDATA1;  %%Cambiar a real
+REFC2=BASEBlackDATA2;
+
+Gain1=BASEBlackDATA1/REFC1;  %change to baseblack data when gain is zero
+Gain2=BASEBlackDATA2/REFC2;  %change to baseblack data when gain is zero
 
 FiltData1=filtfilt(b,a,data1(:,4:e));
 FiltData2=filtfilt(b,a,data2(:,4:e));
@@ -72,18 +81,52 @@ PromCanal2=mean(FiltData2,2);
 PromDATA(:,1:2)=[PromCanal1 , PromCanal2];
 dlmwrite('prueba.csv', PromDATA');
 
+tvect=1000*(1:length(PromCanal1))/fs; 
+
+%Canal 1
+%Find P100
+[Ax1P100,Ix1P100] = min(PromCanal1(20:120)); %%Análisis señal canal 1
+Ix1P100=Ix1P100+20;
+tx1P100=tvect(Ix1P100);
+
+%Find N75 and N145 
+[Ax1N75,Ix175] = max(PromCanal1(Ix1P100-rango:Ix1P100));
+
+tx1N75=tvect(Ix175+Ix1P100-rango);
 
 
+[AxN145,Ix1145] = max(PromCanal1(Ix1P100:Ix1P100+rango));
+tx1N145=tvect(Ix1P100+Ix1145);
+
+
+dly1P100=tx1P100-100;
+
+%Canal 2
+%Find P100
+[Ax2P100,Ix2P100] = min(PromCanal2(20:120)); %%Análisis señal canal 2
+Ix2P100=Ix2P100+20;
+tx2P100=tvect(Ix2P100);
+
+%Find N75 and N145 
+[Ax2N75,Ix275] = max(PromCanal2(Ix2P100-rango:Ix2P100));
+
+tx2N75=tvect(Ix275+Ix2P100-rango);
+
+
+[Ax2N145,Ix2145] = max(PromCanal2(Ix2P100:Ix2P100+rango));
+tx2N145=tvect(Ix2P100+Ix2145);
+
+
+dly2P100=tx2P100-100;
 
 figure
-plot(PromDATA)
-% hold on
-% plot (spromFilt)
-%clear all
-%exit
-Resultados=[ 1 2 3 4 5; 10 20 30 40 50];
+plot(tvect,PromCanal2, tx2P100,Ax2P100,'+',tx2N75, Ax2N75, '+',tx2N145,Ax2N145,'+' )
+figure
+plot(tvect,PromCanal1, tx1P100,Ax1P100,'+',tx1N75, Ax1N75, '+',tx1N145,AxN145,'+' )
 
-dlmwrite('resultados.csv', Resultados);
+Resultados=[Ax1P100 tx1P100  tx1N75 tx1N145  dly1P100; Ax2P100 tx2P100 tx2N75 tx2N145 dly2P100];
+
+dlmwrite('resultados.csv', Resultados); %Agregar ganancia
 clear all
 end
 
--- process_ERP_prueba.m
+++ process_ERP_prueba.m
@@ -18,21 +18,17 @@ wait(s);
 delete(lh);
 
 release(s)
-% pruebas=
-% 
-%  for i=1:1:prueb
-%      cont=250*i;
-%      cont2=i*2;
-%      canal1(cont-249:cont,1)=data(:,cont2-1);
-%      canal2(cont-249:cont,1)=data(:,cont2);
-%  end
-
 
 
 %% Análisis señal 
-
+rango=25;
 PromCanal1=mean(data1,2);
 PromCanal2=mean(data2,2);
+
+REFC1=PromCanal1;  %%Cambiar a 
+REFC2=PromCanal2;
+
+%CANAL 1
 
 BASELINEPromCanal1=mean(PromCanal1);
 
@@ -41,17 +37,26 @@ tvect=1000*(1:length(PromCanal1))/fs;
 PromDATA(:,1:2)=[PromCanal1 , PromCanal2];
 dlmwrite('prueba.csv', PromDATA');
 
+%Find P100
+[Ax1P100,Ix1P100] = min(PromCanal1(20:120)); %%Análisis señal canal 1
+tx1P100=tvect(Ix1P100);
+
+%Find N75 and N145 
+[Ax1N75,Ix175] = max(PromCanal1(Ix1P100-rango:Ix1P100));
+
+tx1N75=tvect(Ix175);
 
 
-[Ax1P100,Ix1P100] = min(PromCanal1(1, 20:120)); 
-tx1P100=tvect(IxP100);
+[AxN145,Ix1145] = max(PromCanal1(Ix1P100:Ix1P100+rango));
+tx1N145=tvect(Ix1P100+Ix1145);
+
+
+dly1P100=tx1P100-100;
 
 
 
 
-
-
-Resultados=[ 1 2 3 4 5; 10 20 30 40 50];
+Resultados=[Ax1P100 tx1P100  tx1N75 tx1N145  dly1P100; 10 20 30 40 50];
 
 dlmwrite('resultados.csv', Resultados);
 clear all
